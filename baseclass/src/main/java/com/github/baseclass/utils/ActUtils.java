b3d0039b47b3a07272bfe3d89004960f4cc614ad
zhongrui_home <1394572279@qq.com>
2017-07-17 22:00:30 +0800

    baseclass

--- baseclass/src/main/java/com/github/baseclass/utils/ActUtils.java
+++ baseclass/src/main/java/com/github/baseclass/utils/ActUtils.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.util.Pair;
-import android.view.View;
 
 /**
  * Created by Administrator on 2017/7/17.
@@ -28,46 +27,52 @@ public class ActUtils {
 
 
 
-    /*********************************带动画的跳转*******************************************/
-    public void STActivityForResult(Activity activity, Class clazz, int requestCode, View[]view,String[]transitionName) {
+    /*********************************带动画的跳转************************************View[]view,String[]transitionName*******/
+    public void STActivityForResult(Activity activity, Class clazz, int requestCode,Pair... pair ) {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            if (view != null) {
+            /*if (view != null) {
                 for (int i = 0; i < view.length; i++) {
-
+                    Pair.create(null,null);
                 }
-            }
+            }*/
             /*
                             Pair.create(holder.getView(R.id.iv_xinyong_card),"content"),
                             Pair.create(holder.getView(R.id.iv_xinyong_card)*/
-            for (View v:view) {
+            /*for (View v:view) {
                 if (v != null) {
                     Pair.create(null,"content");
                 }
-            }
+            }*/
             ActivityOptions options =ActivityOptions
-                    .makeSceneTransitionAnimation(activity,getPair(),"content");
-            activity.startActivity(new options.toBundle());
+                    .makeSceneTransitionAnimation(activity,pair);
+            activity.startActivity(new Intent(activity,clazz), options.toBundle());
         }else{
-            STActivity(CreditCardDetailsActivity.class);
+            STActivity(activity,clazz);
         }
     }
 
 
-    public void STActivityForResult(Activity activity,Intent intent, Class clazz, int requestCode) {
-        intent.setClass(activity, clazz);
-        activity.startActivityForResult(intent, requestCode);
-    }
-    public void STActivity(Activity activity,Class clazz) {
-        activity.startActivity(new Intent(activity, clazz));
-    }
-    public void STActivity(Activity activity,Intent intent, Class clazz) {
+    public void STActivityForResult(Activity activity,Intent intent, Class clazz, int requestCode,Pair... pair) {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            ActivityOptions options =ActivityOptions
-                    .makeSceneTransitionAnimation(getActivity(),
-                            Pair.create(holder.getView(R.id.iv_xinyong_card),"content"),
-                            Pair.create(holder.getView(R.id.iv_xinyong_card),"content")
-                    );
-            activity.startActivity(new options.toBundle());
+            ActivityOptions options =ActivityOptions.makeSceneTransitionAnimation(activity,pair);
+            activity.startActivityForResult(new Intent(activity,clazz), requestCode,options.toBundle());
+        }else{
+            STActivityForResult(activity,clazz,requestCode);
+        }
+    }
+    public void STActivity(Activity activity,Class clazz,Pair... pair) {//非空判断是否能进
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options =ActivityOptions.makeSceneTransitionAnimation(activity,pair);
+            activity.startActivity(new Intent(activity,clazz), options.toBundle());
+        }else{
+            STActivity(activity,clazz);
+        }
+    }
+    public void STActivity(Activity activity,Intent intent, Class clazz,Pair... pair) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            intent.setClass(activity, clazz);
+            ActivityOptions options =ActivityOptions.makeSceneTransitionAnimation(activity,pair);
+            activity.startActivity(intent,options.toBundle());
         }else{
             STActivity(activity,intent,clazz);
         }
